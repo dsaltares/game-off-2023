8e6618926f66968b83781112f024a74741037b24
David Saltares <david.saltares@gmail.com>
2023-11-09 09:12:32 +0100

    fix: ai

--- game/scripts/bouncer.gd
+++ game/scripts/bouncer.gd
@@ -1,10 +1,10 @@
 extends StaticBody3D
 class_name Bouncer
 
-@onready var animation_player: AnimationPlayer = %AnimationPlayer
+@onready var anim_tree: AnimationNodeStateMachinePlayback = %AnimationTree.get('parameters/playback')
 
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if body.has_method('high_jump'):
 		body.high_jump()
-		animation_player.play("bounce")
+		anim_tree.travel("Bounce")

bf4bfbe933f25a9791df57b951303c6671f80dc5
David Saltares <david.saltares@gmail.com>
2023-11-10 10:46:16 +0100

    feat: spikes support delay

--- game/scripts/spikes.gd
+++ game/scripts/spikes.gd
@@ -1,11 +1,15 @@
 extends StaticBody3D
 class_name Spikes
 
+@export var delay := 0.0
+
 @onready var hazard_area := %HazardArea
 @onready var animation_player := %AnimationPlayer
 
-
 func _ready() -> void:
+	if delay > 0.0:
+		await get_tree().create_timer(delay).timeout
+		
 	animation_player.play("idle")
 
 
